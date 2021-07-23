fb9333487be7f5ecd940122f91ea3af5e450bacb
jiazhang <jiazhang@uxon.net.cn>
2021-07-23 18:07:58 +0800

    更新配置读取信息。

---
+++ configuration/configuration.go
@@ -0,0 +1,27 @@
+package configuration
+
+import (
+	"fmt"
+	"github.com/fsnotify/fsnotify"
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	Name string
+}
+
+func Init(cfg string) error {
+	c := Config{
+		Name: cfg,
+	}
+
+	c.watchConfig()
+	return nil
+}
+
+func (c *Config) watchConfig() {
+	viper.WatchConfig()
+	viper.OnConfigChange(func(e fsnotify.Event) {
+		fmt.Printf("Config file changed: %s", e.Name)
+	})
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module goupload
 
 go 1.14
 
-require github.com/gin-gonic/gin v1.7.2
+require (
+	github.com/gin-gonic/gin v1.7.2
+	github.com/spf13/viper v1.8.1
+)
